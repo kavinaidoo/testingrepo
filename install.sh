b24655438b1fc1c5c3e0ce5e3459d24beed7416c
kavinaidoo <83715825+kavinaidoo@users.noreply.github.com>
2023-10-18 15:12:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,19 +11,37 @@ else
     real_user=$(whoami)
 fi
 
-echo "\nUpdate, Upgrade, Install\n"
+echo "\n**** Running apt update and upgrade ****\n"
 
 apt update
 apt upgrade -y
-apt-get -y install --no-install-recommends build-essential git autoconf automake libtool \
-    libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev
+
+echo "\n**** Installing shairport-sync ****\n"
+
+echo "\n* Installing Requirements *\n"
+
+apt install --no-install-recommends build-essential git autoconf automake libtool \
+    libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev \
+    libplist-dev libsodium-dev libavutil-dev libavcodec-dev libavformat-dev uuid-dev libgcrypt-dev xxd
 
 
-#echo "\nRunning as $real_user\n"
+echo "\n* Cloning, making and installing nqptp *\n"
 
-echo "\nCloning and Make\n"
+cd /home/$real_user/
+git clone https://github.com/mikebrady/nqptp.git
+cd /home/$real_user/nqptp
+autoreconf -fi
+./configure --with-systemd-startup
+make
+make install
 
-#sudo -i -u $real_user bash << EOF
+#Not adding nqptp as a service
+#systemctl enable nqptp
+#systemctl start nqptp
+
+
+echo "\n* Cloning, making and installing shairport-sync *\n"
+
 cd /home/$real_user/
 git clone https://github.com/mikebrady/shairport-sync.git
 cd /home/$real_user/shairport-sync
@@ -31,19 +49,9 @@ autoreconf -fi
 ./configure --sysconfdir=/etc --with-alsa \
     --with-soxr --with-avahi --with-ssl=openssl --with-systemd
 make
-#EOF
-
-echo "\nMake Install\n"
-
-#echo "\nRunning as root\n"
-
-#cd /home/$real_user/
 make install
 
-#echo "\nRunning as $real_user\n"
+echo "\n* Run nqptp and shairport-sync *\n"
 
-#sudo -u $real_user 
-
-echo "\nRun shairport-sync\n"
-
-shairport-sync
+#running both simultaneously -> https://stackoverflow.com/a/52033580
+(trap 'kill 0' SIGINT; /home/$real_user/nqptp/nqptp & /home/$real_user/shairport-sync/shairport-sync & wait)
