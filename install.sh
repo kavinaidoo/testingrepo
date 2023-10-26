1232dd9ad7b8f4fca43cfe48d0ea610ab8f33d87
kavinaidoo <83715825+kavinaidoo@users.noreply.github.com>
2023-10-26 21:28:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,13 @@
-#!/bin/bash
+# START setting up pmap as a service ****************************************************
 
-if ! [ $(id -u) = 0 ]; then
-   echo "The script needs to be run as root." >&2
-   exit 1
-fi
+echo "\n**** Setting up pmap as a service ****\n"
 
-if [ $SUDO_USER ]; then
-    real_user=$SUDO_USER
-else
-    real_user=$(whoami)
-fi
+cd /etc/systemd/system/
+curl -O https://raw.githubusercontent.com/kavinaidoo/pmap/dev/pmap.service
 
-echo "\n**** installating ubuntu font ****\n"
+systemctl daemon-reload
+systemctl enable pmap.service
 
-cd /home/$real_user/pmap
+echo "\n**** Setting up pmap as a service completed ****\n"
 
-curl -O https://github.com/google/fonts/raw/main/ufl/ubuntu/Ubuntu-Regular.ttf
-
-mkdir ubuntu_font_docs
-cd /home/$real_user/pmap/ubuntu_font_docs
-
-curl -O https://raw.githubusercontent.com/google/fonts/main/ufl/ubuntu/COPYRIGHT.txt
-curl -O https://raw.githubusercontent.com/google/fonts/main/ufl/ubuntu/TRADEMARKS.txt
-curl -O https://raw.githubusercontent.com/google/fonts/main/ufl/ubuntu/UFL.txt
-
-echo "\n**** installating ubuntu font complete ****\n"
-
-# END pmap installation ****************************************************
-
+# END setting up pmap as a service ****************************************************
